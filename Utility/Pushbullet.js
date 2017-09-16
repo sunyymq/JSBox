551654687a0119c80627afc4b7438c31f98d4ab9
Neurogram-R <greedyage@hotmail.com>
2017-09-16 20:05:24 +0800

    Update Pushbullet.js

--- Utility/Pushbullet.js
+++ Utility/Pushbullet.js
@@ -17,7 +17,7 @@
         if (idx == 0) {
           $http.request({
       method: "GET",
-      url: "https://api.pushbullet.com/v2/pushes&limit=" + pushLimit,
+      url: "https://api.pushbullet.com/v2/pushes?active=true&limit=" + pushLimit,
       header: {
         "Access-Token": apiKey
       },
